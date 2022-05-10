152923a9a817303791900c3688ed8bc5ed9bf8dc
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-11 01:43:24 +0600

    doctor portal web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Review from './Pages/Review/Review';
+import Login from './Pages/Login/Login';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+          <Route path='/review' element={<Review></Review>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+        <Home></Home>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+           <button class="btn bg-primary">Button</button>
+            <h3>Hello heading 3</h3>
+            <h4>Hello heading 4</h4>
+            <h5>Hello heading 5</h5>
+        </div>
+    );
+};
+
+export default Home;
