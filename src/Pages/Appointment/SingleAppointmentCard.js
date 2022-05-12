479e8d3b68ab15aed7e374830448bc1ac128bb1c
mkpbd <mostofakamal1990@gmail.com>
2022-05-12 19:11:49 +0600

    servie json file add and  show data in card

--- src/Pages/Appointment/SingleAppointmentCard.js
+++ src/Pages/Appointment/SingleAppointmentCard.js
@@ -5,10 +5,13 @@ const SingleAppointmentCard = ({service}) => {
         <div className="card bg-base-100 shadow-xl my-4">
         <div className="card-body">
           <h2 className="card-title justify-center text-3xl text-primary">Card title!</h2>
-          <p className='justify-center'>Available Space {service?.slots.length}</p>
-          <p className='justify-center'>If a dog chews shoes whose shoes does he choose?</p>
+            {
+                service?.slots.length ?  <p className='text-center font-bold'>{service?.slots[0]}  </p> :  <p className='text-center text-red-500'>Booking is fulled</p>
+            }
+          <p className='justify-center text-center'>{service?.slots.length} SPACES AVAILABLE </p>
+          <p className='text-center'>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Book Appointment</button>
+            <button disabled={service?.slots.length === 0} className="btn btn-primary">Book Appointment</button>
           </div>
         </div>
       </div>
